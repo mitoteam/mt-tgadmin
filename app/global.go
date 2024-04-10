9ea9294f33acf127d5c91d8c3cab8d403bae49e1
Mikhail Oleynik <mishutka@mito-team.com>
2024-04-10 10:01:12 +0300

    n/s

--- app/global.go
+++ app/global.go
@@ -1,7 +1,18 @@
 // Package app contains main mt-tgadmin functionality.
 package app
 
-import "github.com/mitoteam/goappbase"
+import (
+	"github.com/mitoteam/goappbase"
+)
+
+type AppSettingsType struct {
+	goappbase.AppSettingsBase `yaml:",inline"`
+
+	BotToken  string `yaml:"bot_token" yaml_comment:"Bot authorization token"`
+	BotChatID int64  `yaml:"bot_chat_id" yaml_comment:"Telegram 'chat_id' int64 value"`
+
+	GuiPassword string `yaml:"gui_password" yaml_comment:"GUI access password"`
+}
 
 var (
 	App      *goappbase.AppBase
